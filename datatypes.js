f28f7f830ca8ae1ba1e169216f69762a1693b47d
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-03 10:01:20 +0300

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let FavouriteFruit = 'strawberries';
+let FavouriteIceCream = 'chocolate';
+let FavouriteProgrammingLanguange = 'JavaScript';
+
+
+let NumberofDoughnuts  = 6;
+let pi = 3.14;
+
+
+let VeryLargeNumber = 567889357155497459154n;
+
+let lovesCoding = true;
+
+let FavouriteColor;
+console.log(FavouriteColor);
