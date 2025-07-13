44187874d0effbb49443f942857cea85fc53ccbd
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-13 17:01:39 +0300

    Added a fully functioning grading system

--- grading-system.js
+++ grading-system.js
@@ -1,6 +1,6 @@
 // updated version of the grading system
 
-let students = [
+let Students = [
     {
         name : "Tendo Calvin",
         marks :{
@@ -28,3 +28,29 @@ let students = [
         }  
     }
 ]
+
+for(let student of Students){
+    let marks = Students.marks; 
+    let total = 0;
+    let numSubjects  = 0 ;
+
+
+
+for (let subject in marks){
+    total += marks[subject];
+    numSubjects++;
+
+}
+
+
+
+let average = total / numSubjects;
+let status = average => 60 ? "PASS" : "FAIL";
+
+console.log(`\n${student.name}`);
+console.log(`Marks: ${JSON.stringify(marks)}`);
+console.log(`Average: ${average.toFixed(2)}`);
+console.log(`Status: ${status}`);
+
+
+}
