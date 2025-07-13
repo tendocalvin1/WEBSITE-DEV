852fb2460afc8ba98242239b43be05786d2a9524
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-13 16:48:21 +0300

    Added a file called Grading-System to fully demonstrate how a grading system fully works

---
+++ grading-system.js
@@ -0,0 +1,30 @@
+// updated version of the grading system
+
+let students = [
+    {
+        name : "Tendo Calvin",
+        marks :{
+            calculus: 75,
+            linear_Algebra : 89,
+            probability : 77
+        }
+    },
+
+    {
+        name : "David Beckham",
+        marks :{
+            calculus: 85,
+            linear_Algebra : 80,
+            probability : 76
+        }
+    },
+
+    {
+        name : "Trent Arnold",
+        marks :{
+            calculus: 33,
+            linear_Algebra : 65,
+            probability : 81
+        }  
+    }
+]
