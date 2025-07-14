10cca5b0b4f7ca06bcf5dd4d9c3e6255dce23103
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-14 09:35:08 +0300

    Added a few changes to the variables3.js file

--- variables3.js
+++ variables3.js
@@ -23,5 +23,33 @@ console.log(num);
 
 //Constants
 // To declare constants (unchanging) variables, we use "const" instead of let
+// Variables declared using const are called "Constants". They cannot be reassigned 
 const myBirthday = '30.05.2002';
 console.log(myBirthday);
+
+// UPPER CONSTANTS
+const COLOR_RED = '#F00';
+const COLOR_GREEN = '#0F0';
+const COLOR_BLUE = '#00F';
+const COLOR_ORANGE = '#FF7F00';
+
+// ... when we need to pick a color
+let color = COLOR_GREEN;
+console.log(color);
+
+
+let admin = 'John';
+let name = 'John';
+
+console.log(admin);
+console.log(name);
+
+const planet = "Earth";
+console.log(planet);
+
+const current_visitor = 'CurrentUser';
+console.log(current_visitor);
+
+
+const BIRTHDAY = '18.04.1982';
+console.log(BIRTHDAY);
