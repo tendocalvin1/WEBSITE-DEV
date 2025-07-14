8c23fcd95df2845510fb86f5f21d3e4bb3744c39
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-14 08:54:02 +0300

    Reviewing Variables in JS to have a full understanding of the languange so that I become a better software engineer in 2025

---
+++ variables3.js
@@ -0,0 +1,27 @@
+// A variable is a named storage for data.
+// To create a variable in Js, we use the "let" keyword
+// let message = 'Hello';
+// console.log(message);
+
+let message;
+message = 'Hello';
+message = 'World !';
+console.log(message);
+
+let mail = 'Hello world';
+console.log(mail);
+
+let hello =  'Hello World !';
+console.log(hello);
+
+let $ = 1;
+let _ = 2;
+console.log($ + _);
+
+num = 5;
+console.log(num);
+
+//Constants
+// To declare constants (unchanging) variables, we use "const" instead of let
+const myBirthday = '30.05.2002';
+console.log(myBirthday);
