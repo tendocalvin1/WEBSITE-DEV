dd85b10ddc4d294254071e3df9bf9340879a7258
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-18 09:35:59 +0300

    Made a few changes to this file

--- break-continue-exercises.js
+++ break-continue-exercises.js
@@ -20,11 +20,35 @@
 // Loop through an array of fruits. Stop when you find "banana".
 
 let fruits = ['apples','pineapples','banana','berries','mangoes'];
-for(fruits = 0; fruits <= fruits.length; fruits ++){
-    if(fruits === 'banana'){
-        break;
+for(i = 0; i < fruits.length; i++){
+    
+    if(fruits[i] === 'banana'){
+        console.log("Banana found, stop lopping!");
+        break;  // stop the loop when banana is found
     }else{
-        console.log(fruits);
+        console.log(fruits[i]);
+    }
+}
+
+
+let cars = ['tesla','buggati','rolls-royce','range-rover','masserati'];
+for(i = 0; i < cars.length ; i++){
+    if(cars[i] === 'buggati'){
+        console.log("buggati found!, stop lopping");
+        break;  // stop the loop when buggati is found
+    }else{
+        console.log(cars[i]);
+    }
+}
+
+
+// Loop from 1 to 30. Skip numbers divisible by 3 but print all others
+
+for(i = 1; i <= 30; i++){
+    if(i % 3 === 0){
+        continue;
+    }else{
+        console.log(i);
     }
 }
 
