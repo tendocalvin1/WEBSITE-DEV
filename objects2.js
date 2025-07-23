37f4b2756233eeed3649cd7458f7215ecf7fd29f
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-07-23 12:27:31 +0300

    Revision on objects in JS

--- objects2.js
+++ objects2.js
@@ -1,1 +1,34 @@
-// 
+// object = A collection of related properties and/or methods
+// can represent real world objects(people, products, places)
+//      object = {key, value,
+//                 function()}
+
+const person1 ={
+    firstName: "Tendo",
+    lastName: "Calvin",
+    age: 23,
+    isEmployed: true,
+    sayHello : () =>{console.log("Hello I am Tendo")},
+    eat : () => {console.log("I am eating pork")},
+}
+
+const person2 ={
+    firstName: "David",
+    lastName: "Beckham",
+    age: 50,
+    isEmployed: true,
+    sayHello : () =>{console.log("Hello I am David")},
+    eat : () => {console.log("I am eating pizza")},
+}
+
+console.log(person2.age);
+console.log(person2.lastName);
+console.log(person2.firstName);
+console.log(person2.isEmployed);
+
+(person2.sayHello());
+(person1.sayHello());
+
+(person2.eat());
+(person1.eat());
+
